6d95968d6ba63a3a2702e3f66243252b653751e3
NJUerWmm <183896708@qq.com>
2024-02-12 20:02:37 +0800

    240212_part4

--- NInstruct/inferencer/strategy/__init__.py
+++ NInstruct/inferencer/strategy/__init__.py
@@ -3,19 +3,52 @@ from .keywords.keywords_example import keywords_example
 from .nl2sql.nl2sql_example import nl2sql_example
 from .classification.part3_1classification import part3_1classification
 from .classification.part3_2classification import part3_2classification
+from .classification.part4_classification import *
 from .keywords.part3_1keywords import part3_1keywords
 from .keywords.part3_2keywords import part3_2keywords
+from .keywords.part4_keywords import *
 from .nl2sql.part3_1nl2sql import part3_1nl2sql
 from .nl2sql.part3_2nl2sql import part3_2nl2sql
+from .nl2sql.part4_nl2sql import *
 
 STRATEGIES = {
     'classification_example': {'func':classification_example,'type':'classification'},
     'part3_1classification':{'func':part3_1classification,'type':'classification'},
     'part3_2classification':{'func':part3_2classification,'type':'classification'},
+    'part4_classification_down_industry':{'func':classification_down_industry,'type':'classification'},
+    'part4_classification_sub_industry':{'func':classification_sub_industry,'type':'classification'},
+    'part4_classification_down_industry_conn':{'func':classification_down_industry_conn,'type':'classification'},
+    'part4_classification_sub_industry_conn':{'func':classification_sub_industry_conn,'type':'classification'},
+    'part4_classification_down_public':{'func':classification_down_public,'type':'classification'},
+    'part4_classification_sub_public':{'func':classification_sub_public,'type':'classification'},
+    'part4_classification_down_abroad':{'func':classification_down_abroad,'type':'classification'},
+    'part4_classification_sub_abroad':{'func':classification_sub_abroad,'type':'classification'},
+    'part4_classification_down_financial':{'func':classification_down_financial,'type':'classification'},
+    'part4_classification_sub_financial':{'func':classification_sub_financial,'type':'classification'},
+
     'keywords_example':{'func':keywords_example,'type':'keywords'},
     'part3_1keywords':{'func':part3_1keywords,'type':'keywords'},
     'part3_2keywords':{'func':part3_2keywords,'type':'keywords'},
+    'part4_keywords_down_industry':{'func':keywords_down_industry,'type':'keywords'},
+    'part4_keywords_sub_industry':{'func':keywords_sub_industry,'type':'keywords'},
+    'part4_keywords_down_industry_conn':{'func':keywords_down_industry_conn,'type':'keywords'},
+    'part4_keywords_sub_industry_conn':{'func':keywords_sub_industry_conn,'type':'keywords'},
+    'part4_keywords_down_public':{'func':keywords_down_public,'type':'keywords'},
+    'part4_keywords_sub_public':{'func':keywords_sub_public,'type':'keywords'},
+    'part4_keywords_down_abroad':{'func':keywords_down_abroad,'type':'keywords'},
+    'part4_keywords_sub_abroad':{'func':keywords_sub_abroad,'type':'keywords'},
+    'part4_keywords_down_financial':{'func':keywords_down_financial,'type':'keywords'},
+    'part4_keywords_sub_financial':{'func':keywords_sub_financial,'type':'keywords'},
+
+    'nl2sql_example':{'func':nl2sql_example,'type':'nl2sql'},
     'part3_1nl2sql':{'func':part3_1nl2sql,'type':'nl2sql'},
     'part3_2nl2sql':{'func':part3_2nl2sql,'type':'nl2sql'},
-    'nl2sql_example':{'func':nl2sql_example,'type':'nl2sql'}
+    'part4_nl2sql_down_industry':{'func':nl2sql_down_industry,'type':'nl2sql'},
+    'part4_nl2sql_sub_industry':{'func':nl2sql_sub_industry,'type':'nl2sql'},
+    'part4_nl2sql_down_public':{'func':nl2sql_down_public,'type':'nl2sql'},
+    'part4_nl2sql_sub_public':{'func':nl2sql_sub_public,'type':'nl2sql'},
+    'part4_nl2sql_down_abroad':{'func':nl2sql_down_abroad,'type':'nl2sql'},
+    'part4_nl2sql_sub_abroad':{'func':nl2sql_sub_abroad,'type':'nl2sql'},
+    'part4_nl2sql_down_financial':{'func':nl2sql_down_financial,'type':'nl2sql'},
+    'part4_nl2sql_sub_financial':{'func':nl2sql_sub_financial,'type':'nl2sql'}
 }
--- NInstruct/inferencer/strategy/nl2sql/part4_nl2sql.py
+++ NInstruct/inferencer/strategy/nl2sql/part4_nl2sql.py
@@ -4,7 +4,7 @@ from typing import List, Any, Dict
 from utils import make_data_dict, ID_COUNTER, LOGGER
 # from configs import IMG_SAVE_PATH
 
-# 主要向下控股公司所在行业是什么 持股比例>0.3
+# 主要向下控股公司所在行业是什么 持股比例>0.3 反向查询
 def nl2sql_down_industry(
     data: Dict[str, Any],
     what_are_components_flat_skipped_keys: List[str] = [],
@@ -21,7 +21,7 @@ def nl2sql_down_industry(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“公司{data[i]}主要向下控股公司所属的行业是什么”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.行业名称 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称={data[i]} OR b.股票代码={data[i]} OR b.股票代码={data[i][0:6]}) AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.行业名称 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称='{data[i]}' OR b.股票代码='{data[i]}' OR b.股票代码='{data[i][0:6]}') AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
                 ]
             )
         )
@@ -46,7 +46,7 @@ def nl2sql_sub_industry(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“公司{data[i]}主要的子公司所在的行业是什么”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.行业名称 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称={data[i]} OR b.股票代码={data[i]} OR b.股票代码={data[i][0:6]}) AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.行业名称 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称='{data[i]}' OR b.股票代码='{data[i]}' OR b.股票代码='{data[i][0:6]}') AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
                 ]
             )
         )
@@ -55,6 +55,7 @@ def nl2sql_sub_industry(
     return results
 
 '''子公司和原公司业务领域有何关联？'''
+# 业务关联表暂缺？
 
 # 向下控股公司是否存在纠纷问题 此处包括联营合营 直接筛选诉讼仲裁内容
 def nl2sql_down_public(
@@ -73,7 +74,7 @@ def nl2sql_down_public(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“公司{data[i]}的向下控股公司是否存在纠纷问题”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT a.公告日期, a.事件内容 FROM 诉讼仲裁 AS a WHERE ((a.证券简称={data[i]} OR a.证券代码={data[i]} OR a.证券代码={data[i][0:6]}) AND ((a.事件内容 LIKE '%子公司%') OR (a.事件内容 LIKE '%联营%') OR (a.事件内容 LIKE '%合营%'))) ORDER BY a.公告日期 ASC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT a.公告日期, a.事件内容 FROM 诉讼仲裁 AS a WHERE ((a.证券简称='{data[i]}' OR a.证券代码='{data[i]}' OR a.证券代码='{data[i][0:6]}') AND ((a.事件内容 LIKE '%子公司%') OR (a.事件内容 LIKE '%联营%') OR (a.事件内容 LIKE '%合营%'))) ORDER BY a.公告日期 ASC;  \n```"
                 ]
             )
         )
@@ -98,7 +99,7 @@ def nl2sql_sub_public(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“公司{data[i]}的子公司是否存在纠纷问题”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT a.公告日期, a.事件内容 FROM 诉讼仲裁 AS a WHERE ((a.证券简称={data[i]} OR a.证券代码={data[i]} OR a.证券代码={data[i][0:6]}) AND (a.事件内容 LIKE '%子公司%')) ORDER BY a.公告日期 ASC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT a.公告日期, a.事件内容 FROM 诉讼仲裁 AS a WHERE ((a.证券简称='{data[i]}' OR a.证券代码='{data[i]}' OR a.证券代码='{data[i][0:6]}') AND (a.事件内容 LIKE '%子公司%')) ORDER BY a.公告日期 ASC;  \n```"
                 ]
             )
         )
@@ -123,7 +124,7 @@ def nl2sql_down_abroad(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“公司{data[i]}主要向下控股公司是否为海外实体”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.注册具体地址 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称={data[i]} OR b.股票代码={data[i]} OR b.股票代码={data[i][0:6]}) AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.注册具体地址 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称='{data[i]}' OR b.股票代码='{data[i]}' OR b.股票代码='{data[i][0:6]}') AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
                 ]
             )
         )
@@ -148,7 +149,7 @@ def nl2sql_sub_abroad(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“公司{data[i]}主要子公司是否为海外实体”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.注册具体地址 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称={data[i]} OR b.股票代码={data[i]} OR b.股票代码={data[i][0:6]}) AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, b.注册具体地址 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称='{data[i]}' OR b.股票代码='{data[i]}' OR b.股票代码='{data[i][0:6]}') AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
                 ]
             )
         )
@@ -173,7 +174,7 @@ def nl2sql_down_financial(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“请列举公司{data[i]}主要向下控股公司的重要财务信息”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, t1.流动比率, t1.速动比率, t1.营运资金, t1.利息保障倍数, t1.资产负债率, t1.权益乘数, t2.可持续增长率, t3.净资产收益率（ROE）A, t4.应收账款周转率A, t4.存货周转率A, t4.应付账款周转率 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称={data[i]} OR b.股票代码={data[i]} OR b.股票代码={data[i][0:6]}) AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) JOIN 偿债能力 AS t1 ON (t.统计截止日期=t1.统计截止日期 AND t.证券代码=t1.股票代码) JOIN 发展能力 AS t2 ON (t.统计截止日期=t2.统计截止日期 AND t.证券代码=t2.股票代码) JOIN 盈利能力 AS t3 ON (t.统计截止日期=t3.统计截止日期 AND t.证券代码=t3.股票代码) JOIN 经营能力 AS t4 ON (t.统计截止日期=t4.统计截止日期 AND t.证券代码=t4.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, t1.流动比率, t1.速动比率, t1.营运资金, t1.利息保障倍数, t1.资产负债率, t1.权益乘数, t2.可持续增长率, t3.净资产收益率（ROE）A, t4.应收账款周转率A, t4.存货周转率A, t4.应付账款周转率 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称='{data[i]}' OR b.股票代码='{data[i]}' OR b.股票代码='{data[i][0:6]}') AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) JOIN 偿债能力 AS t1 ON (t.统计截止日期=t1.统计截止日期 AND t.证券代码=t1.股票代码) JOIN 发展能力 AS t2 ON (t.统计截止日期=t2.统计截止日期 AND t.证券代码=t2.股票代码) JOIN 盈利能力 AS t3 ON (t.统计截止日期=t3.统计截止日期 AND t.证券代码=t3.股票代码) JOIN 经营能力 AS t4 ON (t.统计截止日期=t4.统计截止日期 AND t.证券代码=t4.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
                 ]
             )
         )
@@ -198,7 +199,7 @@ def nl2sql_sub_financial(
                 cur_id=str(ID_COUNTER),
                 cur_conversations=[
                     f"请根据以下用户输入，输出sql代码。\n用户输入：“请列举公司{data[i]}主要子公司的重要财务信息”",
-                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, t1.流动比率, t1.速动比率, t1.营运资金, t1.利息保障倍数, t1.资产负债率, t1.权益乘数, t2.可持续增长率, t3.净资产收益率（ROE）A, t4.应收账款周转率A, t4.存货周转率A, t4.应付账款周转率 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称={data[i]} OR b.股票代码={data[i]} OR b.股票代码={data[i][0:6]}) AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) JOIN 偿债能力 AS t1 ON (t.统计截止日期=t1.统计截止日期 AND t.证券代码=t1.股票代码) JOIN 发展能力 AS t2 ON (t.统计截止日期=t2.统计截止日期 AND t.证券代码=t2.股票代码) JOIN 盈利能力 AS t3 ON (t.统计截止日期=t3.统计截止日期 AND t.证券代码=t3.股票代码) JOIN 经营能力 AS t4 ON (t.统计截止日期=t4.统计截止日期 AND t.证券代码=t4.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
+                    f"根据用户输入问题，编写sql代码如下：\n```sql\n SELECT t.统计截止日期, t.证券代码, b.股票简称, t.持股比例, t1.流动比率, t1.速动比率, t1.营运资金, t1.利息保障倍数, t1.资产负债率, t1.权益乘数, t2.可持续增长率, t3.净资产收益率（ROE）A, t4.应收账款周转率A, t4.存货周转率A, t4.应付账款周转率 FROM (SELECT a.统计截止日期, a.证券代码, a.持股比例 FROM 十大股东 AS a JOIN 上市公司基本信息年度表 AS b ON (a.统计截止日期=b.统计截止日期 AND (a.股东名称=b.中文全称 OR a.股东名称=b.股票简称) AND (b.股票简称='{data[i]}' OR b.股票代码='{data[i]}' OR b.股票代码='{data[i][0:6]}') AND a.持股比例>0.3)) AS t JOIN 上市公司基本信息年度表 AS b ON (t.统计截止日期=b.统计截止日期 AND t.证券代码=b.股票代码) JOIN 偿债能力 AS t1 ON (t.统计截止日期=t1.统计截止日期 AND t.证券代码=t1.股票代码) JOIN 发展能力 AS t2 ON (t.统计截止日期=t2.统计截止日期 AND t.证券代码=t2.股票代码) JOIN 盈利能力 AS t3 ON (t.统计截止日期=t3.统计截止日期 AND t.证券代码=t3.股票代码) JOIN 经营能力 AS t4 ON (t.统计截止日期=t4.统计截止日期 AND t.证券代码=t4.股票代码) ORDER BY t.统计截止日期 ASC AND t.持股比例 DESC;  \n```"
                 ]
             )
         )
